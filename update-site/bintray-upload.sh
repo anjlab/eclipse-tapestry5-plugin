29f4a2f856d0018e0f4fcca7da3617191f083359
dmitrygusev <dmitry.gusev@gmail.com>
2013-09-09 22:51:31 +0400

    Move to new update site

--- update-site/bintray-upload.sh
+++ update-site/bintray-upload.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-PACKAGE_NAME=eclipse-tapestry5-plugin
+REPO_NAME=eclipse-tapestry5-plugin
+PACKAGE_NAME=update-site
 VERSION_NAME=1.1.2
 
 curl -vT content.jar \
     -udmitrygusev:$bintray_api_key \
-    https://api.bintray.com/content/anjlab/eclipse/$PACKAGE_NAME/$VERSION_NAME/
+    https://api.bintray.com/content/anjlab/$REPO_NAME/$PACKAGE_NAME/$VERSION_NAME/
 
 curl -vT artifacts.jar \
     -udmitrygusev:$bintray_api_key \
-    https://api.bintray.com/content/anjlab/eclipse/$PACKAGE_NAME/$VERSION_NAME/
+    https://api.bintray.com/content/anjlab/$REPO_NAME/$PACKAGE_NAME/$VERSION_NAME/
 
 curl -vT features/com.anjlab.eclipse.tapestry5.feature_$VERSION_NAME.jar \
     -udmitrygusev:$bintray_api_key \
-    https://api.bintray.com/content/anjlab/eclipse/$PACKAGE_NAME/$VERSION_NAME/features/
+    https://api.bintray.com/content/anjlab/$REPO_NAME/$PACKAGE_NAME/$VERSION_NAME/features/
 
 curl -vT plugins/com.anjlab.eclipse.tapestry5_$VERSION_NAME.jar \
     -udmitrygusev:$bintray_api_key \
-    https://api.bintray.com/content/anjlab/eclipse/$PACKAGE_NAME/$VERSION_NAME/plugins/
+    https://api.bintray.com/content/anjlab/$REPO_NAME/$PACKAGE_NAME/$VERSION_NAME/plugins/
 
