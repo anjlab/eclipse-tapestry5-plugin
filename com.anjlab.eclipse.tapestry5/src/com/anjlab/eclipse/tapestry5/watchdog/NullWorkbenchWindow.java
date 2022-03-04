2e8a14da1a047812bf8263ef59ffdd1bb24e21a7
Dmitry Gusev <dmitry.gusev@satago.com>
2022-03-04 22:23:51 +0000

    Prepare for merge

--- com.anjlab.eclipse.tapestry5/src/com/anjlab/eclipse/tapestry5/watchdog/NullWorkbenchWindow.java
+++ com.anjlab.eclipse.tapestry5/src/com/anjlab/eclipse/tapestry5/watchdog/NullWorkbenchWindow.java
@@ -64,5 +64,6 @@ public class NullWorkbenchWindow implements IWorkbenchWindow
 
     @Override public boolean close() { return false; }
 
-	@Override public boolean isClosing() { return false; }
+    @Override public boolean isClosing() { return false; }
+
 }
